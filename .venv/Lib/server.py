4d5e155811666e80368c5b01a2e57b416263297d
Tamanda4 <t6433175l@gmail.com>
2025-06-10 09:27:05 +0300

    "added function to start server"

--- .venv/Lib/server.py
+++ .venv/Lib/server.py
@@ -8,3 +8,16 @@ PORT = 12345
 clients = []
 server_running = False
 server_socket = None
+
+def start_server():
+    global server_socket, server_running
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+        log("[SERVER STARTED] Listening on {}:{}".format(HOST, PORT))
+        server_running = True
+        threading.Thread(target=accept_clients, daemon=True).start()
+    except Exception as e:
+        log(f"[ERROR] {e}")
+
